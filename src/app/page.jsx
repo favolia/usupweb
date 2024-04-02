0816bc24fe080f59661da301c0b88bfe747a298b
favolia <daisszo1455@gmail.com>
2024-04-02 17:56:46 +0800

    add love

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { FaRegHandScissors } from "react-icons/fa6";
 import { IoThumbsUpOutline } from "react-icons/io5";
+import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 
 export default function Home() {
   const [gameInfo, setGameInfo] = useState('GUA SAYANG BANGET SAMA UNI BODOAMAT MAU JERAWATAN, KAGA PERNAH JELEK');
@@ -37,25 +38,29 @@ export default function Home() {
 
   return (
     <main className="w-full h-screen flex flex-col justify-center items-center">
-      <p className="text-6xl text-center">
+      <p className="text-5xl text-center">
         GUA SAYANG BANGET SAMA UNI BODOAMAT MAU JERAWATAN, KAGA PERNAH JELEK
       </p>
       <div className="flex justify-center items-center gap-x-5">
-        <div className="">
-          <Thumbs />
-        </div>
-
-        <div className="font-bold">
-          VS
-        </div>
 
         <div className="scale-x-[-1]">
-          <Thumbs />
+          <FcLikePlaceholder fontSize={70} />
+        </div>
+
+        <div className="">
+          <FcLike fontSize={70} />
+        </div>
+        {/* <div className="font-bold">
+          lope
+        </div> */}
+
+        <div className="scale-x-[-1]">
+          <FcLikePlaceholder fontSize={70} />
         </div>
       </div>
-      <button className="px-4 py-2 rounded-md text-white bg-black" onClick={() => suit()}>
+      {/* <button className="px-4 py-2 rounded-md text-white bg-black" onClick={() => suit()}>
         play
-      </button>
+      </button> */}
     </main>
   );
 }
